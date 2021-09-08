0e5638346d35c9a276d7a95a16abe6858508192d
fuaberu <kevinfabel@gmail.com>
2021-09-08 19:44:46 -0300

    Updates

---
+++ index.ed273923.js
@@ -0,0 +1,2 @@
+function openPopup(e){document.getElementById(e).style.display="block"}function closeForm(e){document.getElementById(e).style.display="none"}
+//# sourceMappingURL=index.ed273923.js.map
